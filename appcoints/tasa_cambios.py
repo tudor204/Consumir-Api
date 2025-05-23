761bc6ddaca99b74ee74440e649599a39bac12f8
Tudor <tudor204@hotmail.com>
2025-05-23 12:50:42 +0200

    prueba

---
+++ appcoints/tasa_cambios.py
@@ -0,0 +1,16 @@
+import requests
+
+#invocar al método get con la url específica
+r = requests.get("https://rest.coinapi.io/v1/exchangerate/BTC/EUR?apikey=1da147e1-614b-4a9d-864f-74bcbd0134b0")
+
+print("codigo http de respuesta: ",r.status_code)
+
+print("cabecera: ",r.headers['content-type'])
+
+print("enconding: ",r.encoding)
+
+print("respuesta en str: ",r.text)
+print(type(r.text))
+
+print("respuesta en json: ",r.json())
+print(type(r.json()))
