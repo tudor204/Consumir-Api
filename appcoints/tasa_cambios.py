48d0d2e53049582dc20a2e73e7eec28692c31147
Tudor <tudor204@hotmail.com>
2025-05-25 10:23:05 +0200

    Añadir clave Key

--- appcoints/tasa_cambios.py
+++ appcoints/tasa_cambios.py
@@ -1,6 +1,7 @@
 import requests
 
 #invocar al método get con la url específica
+#dirección con clave key válida
 r = requests.get("https://rest.coinapi.io/v1/exchangerate/BTC/EUR?apikey=1da147e1-614b-4a9d-864f-74bcbd0134b0")
 
 print("codigo http de respuesta: ",r.status_code)
