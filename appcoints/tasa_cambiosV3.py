dd8b1aaa9bd32cce1a4255caf50e186934c6e0eb
Tudor <tudor204@hotmail.com>
2025-05-27 10:17:28 +0200

    crear nueva tasa cambios con capa modelo

--- appcoints/tasa_cambiosV3.py
+++ appcoints/tasa_cambiosV3.py
@@ -5,13 +5,11 @@ from models import *
 allcoin = AllCoinApiIo()
 
 #consulta de todas las monedas
-
 allcoin.getAllcoins(APIKEY)
 
 
 print(f"La cantidad de criptos son: {len(allcoin.lista_criptos)}.\
         y la cantidad de no criptos son: {len(allcoin.lista_no_criptos)}")
-
 
 ############################################################
         
@@ -19,12 +17,13 @@ moneda_cripto = input("Ingrese una criptomoneda válida ").upper()
 
 while moneda_cripto != "" and moneda_cripto.isalpha() == True:  
     if moneda_cripto in allcoin.lista_criptos:
-        r = requests.get(f"https://rest.coinapi.io/v1/exchangerate/{moneda_cripto}/EUR?apikey={APIKEY}")
-        respuesta = r.json()
-        if r.status_code == 200:
-            print("{:,.2f}€".format(respuesta["rate"]))
-            break
-        else:
-            print(respuesta["error"])
+        exchange = Exchange(moneda_cripto)
+        try:
+            exchange.updateExchange(APIKEY)
+            print("{:,.2f}€".format(exchange.rate).replace(",","@").replace(".",",").replace("@","."))
+        except ModelError as error:
+            print(error)   
+
+    
     moneda_cripto = input("Ingrese una criptomoneda válida ").upper()  
 
